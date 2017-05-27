f4074b1135e515455179eb1696e3bbfbb72083f8
Danilo de Lima <danilodelyima@gmail.com>
2017-05-27 16:16:02 -0300

    first commit

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,39 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());    
+        this._limpa_formulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DataHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpa_formulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+    }
+}
+
---
+++ client/js/app/helpers/DataHelper.js
@@ -0,0 +1,19 @@
+class DataHelper {
+
+	constructor() {
+
+		throw new Error('Date Helper não pode ser instanciado');
+	}
+
+	static textoParaData(texto) {
+
+		if(! /\d{4}-\d{2}-\d{2}/.test(texto))
+			throw new Error('Deve estar no formato aaaa-mm-dd');
+		return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+	}
+
+	static DataParaTexto(data) {
+
+		return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}` 
+	}
+}
